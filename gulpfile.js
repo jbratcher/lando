2966ecb717f44c8d50b011bd389c26dbdadec980
Jeremy Bratcher <jeremybratcher@gmail.com>
2017-11-25 17:45:47 +0000

    file clean up

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,15 @@
 // Import packages
 
-const gulp = require("gulp");
-const browserSync = require("browser-sync").create();
-const sass =    require("gulp-sass");
+const gulp          = require("gulp");
+const browserSync   = require("browser-sync").create();
+const sass          = require("gulp-sass");
 
 // Create tasks
 
 // Complie Sass and inject into browser
 
 gulp.task('sass', function() {
-    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
+    return gulp.src(['src/scss/*.scss'])
         .pipe(sass())
         .pipe(gulp.dest('src/css'))
         .pipe(browserSync.stream());
@@ -18,7 +18,7 @@ gulp.task('sass', function() {
 // Move js file to src/js
 
 gulp.task('js', function() {
-    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
+    return gulp.src(['src/js/*.js'])
         .pipe(gulp.dest('src/js'))
         .pipe(browserSync.stream());
 });
@@ -27,11 +27,11 @@ gulp.task('js', function() {
 
 gulp.task('serve', ['sass'], function() {
     browserSync.init({
-        server: "./src",
+        server: "./",
         port: 8082
     });
     
-    gulp.watch(['node_modules/bootstrap/scss.bootstrap.scss', 'src/scss/*.scss'], ['sass']);
+    gulp.watch(['src/scss/*.scss'], ['sass']);
     gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
